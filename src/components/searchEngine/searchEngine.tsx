ce82790314e332e4d4573393572107de75a08a24
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-04-08 19:18:08 +0200

    layout wyszukiwarki

--- src/components/searchEngine/searchEngine.tsx
+++ src/components/searchEngine/searchEngine.tsx
@@ -4,18 +4,24 @@ import { Icon } from "../ui/Icon/Icon";
 interface SearchEngine {}
 
 /**
- *
+ * Główna wyszukiwarka ofert i profili
  */
 export const SearchEngine = ({}: SearchEngine) => {
   return (
-    <div className="w-[500px] border">
-      <div className="flex w-[300px] flex-col rounded-lg bg-white px-4 font-normal">
-        <input type="text" placeholder="Enter any tag, category or name" />
-        <div> tagi </div>
+    <div className="flex w-[500px] gap-4">
+      <div className="flex min-h-10 w-[300px] flex-col justify-center rounded-lg bg-white px-4 font-normal">
+        <input
+          className="h-10"
+          type="text"
+          placeholder="Enter any tag, category or name"
+        />
+        <div hidden>tags</div>
       </div>
-      <div>lokalizacja</div>
-      <button>
-        <Icon style="w-4 h-4 text-white" name="magnifier" fill="#FFFFFF" />
+      <button className="font-lg h-10 items-center rounded-lg bg-white px-4 text-lg">
+        Location
+      </button>
+      <button className="h-10">
+        <Icon style="w-8 h-8 text-white" name="magnifier" fill="#FFFFFF" />
       </button>
     </div>
   );

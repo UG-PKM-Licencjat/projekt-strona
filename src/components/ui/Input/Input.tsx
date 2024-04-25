b2c9ab71897e8ebad2266b8964b3262ec2337a9a
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-04-25 16:17:54 +0200

    podstawowy input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -10,7 +10,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "rounded-md border border-blue-950 px-8 py-2 outline-none focus:border-2",
           className,
         )}
         ref={ref}

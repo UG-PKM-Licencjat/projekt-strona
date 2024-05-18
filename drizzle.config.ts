0056e072b95d14ab216f46de8ef4077cd5a43aef
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-18 17:44:32 +0200

    Update drizzle-kit and drizzle-orm to latest version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env.js";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    url: env.DATABASE_URL,
   },
   tablesFilter: ["projekt-strona_*"],
-} satisfies Config;
+});

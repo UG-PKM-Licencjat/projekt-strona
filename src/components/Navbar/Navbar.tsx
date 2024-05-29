f5029bbd7ac6692cb10181a27da9ef654f60073d
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-29 23:20:06 +0200

    Make Navbar sticky

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 // Placeholder - modify if needed
 
 export const Navbar = () => (
-  <div className="flex w-full items-center justify-between bg-pink-700 px-10 py-4">
+  <div className="sticky top-0 z-50 flex w-full items-center justify-between bg-pink-700 px-10 py-4">
     <div className="flex items-center gap-8 font-semibold text-white">
       <Link href="/">Home</Link>
       <Link href="#">Offers</Link>

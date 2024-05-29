00723fcd9ea710bd67096cdd238608ed2a70c1ab
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-29 23:20:23 +0200

    Add correct offer page layout

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -5,24 +5,27 @@ import { Offer } from "~/components/Offer/Offer";
 
 const placeholderOffers = [
   {
-    name: "Pizza",
-    description: "Pizza w pieśni",
-    tags: ["Pizza", "Pie", "Pizza", "Pizza"],
+    name: "IMIE NAZWISKO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    tags: ["hashtag", "hashtag", "hashtag", "hashtag"],
   },
   {
-    name: "Pizza",
-    description: "Pizza w pieśni",
-    tags: ["Pizza", "Pie", "Pizza", "Pizza"],
+    name: "IMIE NAZWISKO",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id repudiandae similique aperiam optio earum quidem, ad tempore necessitatibus suscipit iusto?",
+    tags: ["hashtag", "hashtag", "hashtag", "hashtag"],
   },
   {
-    name: "Cake",
-    description: "Kakao w pieśni",
-    tags: ["Cake", "Cake", "Cake", "Cake"],
+    name: "IMIE NAZWISKO",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id repudiandae similique aperiam optio earum quidem, ad tempore necessitatibus suscipit iusto?",
+    tags: ["hashtag", "hashtag", "hashtag", "hashtag"],
   },
   {
-    name: "Pasta",
-    description: "Pasta w pieśni",
-    tags: ["Pasta", "Pasta", "Pasta", "Pasta"],
+    name: "IMIE NAZWISKO",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione alias animi nostrum quo, ipsa doloribus aut a error laborum assumenda eius harum quia, dolor libero. Possimus iusto ullam animi voluptatum distinctio enim suscipit magnam, quaerat omnis quae vitae veritatis laborum reiciendis aliquid dolor quisquam dignissimos accusantium vero dolorum similique cum. Doloremque mollitia sunt voluptates error doloribus, delectus nulla dolorum incidunt, recusandae iusto assumenda deserunt harum, culpa temporibus! Quam nisi corrupti accusantium nostrum dolorum eligendi. Dolorum eaque illo asperiores aperiam nisi delectus eligendi voluptate odit unde! Quisquam, eius! Exercitationem reprehenderit aperiam sit quos accusamus dicta, fugiat sunt possimus. Sed, quibusdam numquam!",
+    tags: ["hashtag", "hashtag", "hashtag", "hashtag"],
   },
 ];
 
@@ -33,7 +36,7 @@ export default function Page() {
       <div className="flex w-full flex-col items-center gap-32 bg-white p-10">
         <div className="flex flex-col items-start gap-20">
           <h1 className="text-6xl font-semibold text-blue-950">ARTYŚCI-APKA</h1>
-          <div className="flex items-center gap-10 font-semibold">
+          <div className="flex items-center gap-20 font-semibold">
             <div className="flex items-center rounded-lg border-2 border-blue-950">
               <input
                 type="text"
@@ -51,14 +54,14 @@ export default function Page() {
           </div>
         </div>
         {/* Offers container */}
-        <div className="flex w-full flex-wrap items-center justify-center gap-2">
+        <div className="grid max-w-[75rem] items-center justify-center gap-2 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-5">
           {placeholderOffers.map((offer, index) => (
             <Offer
               key={index}
               name={offer.name}
               description={offer.description}
               tags={offer.tags}
-              className="basis-1/4"
+              className={index % 3 === 0 ? "xl:col-span-3" : "xl:col-span-2"}
             />
           ))}
         </div>

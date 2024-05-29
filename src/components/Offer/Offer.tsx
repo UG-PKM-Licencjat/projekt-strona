5f0dd997ed7e31761821471134c92a9bc6cce169
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-29 22:14:35 +0200

    Tweak offer colors, change tag key and add className concatenation support

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -1,26 +1,34 @@
 import React from "react";
+import { cn } from "~/utils/cn";
 
 interface OfferProps {
   name: string;
   tags: string[];
   description: string;
+  className?: string;
 }
 
-export const Offer = ({ name, description, tags }: OfferProps) => (
-  <div className="m-4 flex flex-col items-start justify-center gap-4 rounded-lg border-2 border-pink-700 px-10 py-6">
+/**
+ * Pojedyncza karta oferty
+ */
+export const Offer = ({ name, description, tags, className }: OfferProps) => (
+  <div
+    className={cn(
+      "flex flex-col items-start justify-center gap-4 rounded-lg border-2 border-pink-700 bg-white px-10 py-6 shadow-md hover:bg-gray-100",
+      className,
+    )}
+  >
     <div className="flex items-center justify-center gap-6">
       {/* Image placeholder */}
       <div className="h-24 w-24 flex-shrink-0 rounded-full bg-gray-200 bg-cover bg-center" />
       <div className="flex flex-col items-start justify-around">
-        <div className="text-3xl font-bold uppercase text-slate-600">
-          {name}
-        </div>
-        <p className=" font-bold text-slate-500">placeholder</p>
+        <div className="text-3xl font-bold uppercase text-blue-950">{name}</div>
+        <p className=" font-bold text-slate-600">placeholder</p>
         <div className="flex flex-wrap gap-2">
-          {tags.map((tag) => (
+          {tags.map((tag, index) => (
             <div
-              key={tag}
-              className="cursor-pointer rounded bg-purple-400 px-2 py-[0.1rem] text-xs font-semibold text-white hover:bg-purple-500"
+              key={index}
+              className="cursor-pointer rounded bg-blue-900/60 px-2 py-[0.1rem] text-xs font-semibold text-white hover:bg-blue-900/40"
             >
               {tag}
             </div>

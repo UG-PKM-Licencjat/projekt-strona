b6b814b3debb3aa0c69f9808ce2313b73799d435
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-29 23:19:53 +0200

    Tweak Offer styles

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -14,7 +14,7 @@ interface OfferProps {
 export const Offer = ({ name, description, tags, className }: OfferProps) => (
   <div
     className={cn(
-      "flex flex-col items-start justify-center gap-4 rounded-lg border-2 border-pink-700 bg-white px-10 py-6 shadow-md hover:bg-gray-100",
+      "flex h-full max-h-[20rem] flex-col items-start justify-around gap-4 rounded-lg border-2 border-pink-700 bg-white px-8 py-8 shadow-md hover:bg-gray-100",
       className,
     )}
   >
@@ -23,12 +23,12 @@ export const Offer = ({ name, description, tags, className }: OfferProps) => (
       <div className="h-24 w-24 flex-shrink-0 rounded-full bg-gray-200 bg-cover bg-center" />
       <div className="flex flex-col items-start justify-around">
         <div className="text-3xl font-bold uppercase text-blue-950">{name}</div>
-        <p className=" font-bold text-slate-600">placeholder</p>
+        <p className="text-lg font-bold text-slate-600">placeholder</p>
         <div className="flex flex-wrap gap-2">
           {tags.map((tag, index) => (
             <div
               key={index}
-              className="cursor-pointer rounded bg-blue-900/60 px-2 py-[0.1rem] text-xs font-semibold text-white hover:bg-blue-900/40"
+              className="cursor-pointer rounded bg-blue-900/60 px-3 py-[0.1rem] text-sm font-semibold text-white hover:bg-blue-900/40"
             >
               {tag}
             </div>
@@ -36,9 +36,9 @@ export const Offer = ({ name, description, tags, className }: OfferProps) => (
         </div>
       </div>
     </div>
-    <div>
+    <div className="flex flex-col items-start justify-start gap-2 overflow-y-clip">
       <h1 className="text-2xl font-semibold text-slate-500">O MNIE</h1>
-      <div className="font-semibold">{description}</div>
+      <div className="break-words font-semibold">{description}</div>
     </div>
   </div>
 );

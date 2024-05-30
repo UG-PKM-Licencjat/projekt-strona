66b2131637d9a2dd78c2c86596e6def6020d4b31
Magda <111302737+magda1231@users.noreply.github.com>
2024-05-30 12:57:41 +0200

    added auth_middleware for trpc

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,19 @@
 import { initTRPC } from "@trpc/server";
+import { getServerAuthSession } from "./auth";
+import type { Session } from "next-auth";
+import { is } from "drizzle-orm";
 
 const t = initTRPC.create();
+
+export const authMiddleware = t.middleware(async ({ next }) => {
+  const session: Session | null = await getServerAuthSession();
+  console.log("trpc authorization", session);
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  return next();
+});
+export const authedProcedure = t.procedure.use(authMiddleware);
 
 export const router = t.router;
 export const procedure = t.procedure;

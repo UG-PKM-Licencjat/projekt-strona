e4c7cf08d4bd028641f5505ff281b9e5339b3e54
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-02 22:53:22 +0200

    Add checkmark icon

--- src/components/ui/SvgSymbols/SvgSymbols.tsx
+++ src/components/ui/SvgSymbols/SvgSymbols.tsx
@@ -27,6 +27,10 @@ const SvgSymbols = (
     <symbol id="arrow-down">
       <path d="m6 9 6 6 6-6" />
     </symbol>
+    <symbol id="badge-check">
+      <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+      <path d="m9 12 2 2 4-4" />
+    </symbol>
   </svg>
 );
 

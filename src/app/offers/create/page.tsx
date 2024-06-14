b861608971051aa3c594e5a52cecf67679a3fbfe
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-14 21:17:01 +0200

    Add price input with validation and WIP tag input system

--- src/app/offers/create/page.tsx
+++ src/app/offers/create/page.tsx
@@ -2,19 +2,95 @@
 
 import React from "react";
 import { Icon } from "~/components/ui/Icon/Icon";
-import { Tag } from "~/components/Tag/Tag";
-// import { redirect } from "next/navigation";
 import { OfferSegment } from "~/components/ui/OfferSegment/OfferSegment";
-import { StarRating } from "~/components/ui/StarRating/StarRating";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  // SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { useToast } from "~/components/ui/use-toast";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "~/components/ui/popover";
+import { ScrollArea } from "~/components/ui/scroll-area";
+
+import { useForm, useFieldArray } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { DevTool } from "@hookform/devtools";
+
+const offerSchema = z
+  .object({
+    price: z
+      .number({
+        message: "Cena powinna być podana w formacie 123456.50",
+      })
+      .step(0.01, {
+        message: "Cena powinna być podana w formacie 123456.50",
+      })
+      .positive({
+        message: "Cena powinna być podana w formacie 123456.50",
+      })
+      .max(999999999, {
+        message: "Cena nie może przekraczać 999 999 999 zł",
+      }),
+    tags: z
+      .array(z.object({ name: z.string(), id: z.string() }).required())
+      .nonempty({ message: "Musisz dodać co najmniej jeden tag" }),
+  })
+  .required();
+
+type FormData = z.infer<typeof offerSchema>;
 // import { trpc } from "~/app/_trpc/client";
 // import Image from "next/image";
 
 export default function OfferPage() {
+  const { toast } = useToast();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(offerSchema),
+    defaultValues: {
+      tags: [
+        { name: "hashtag1", id: "0" },
+        { name: "hashtag2", id: "1" },
+        { name: "hashtag3", id: "2" },
+      ],
+    },
+  });
+
+  const [tagOpen, setTagOpen] = React.useState(false);
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "tags",
+  });
+
   const tags: { name: string; id: string }[] | undefined = [
     { name: "hashtag1", id: "0" },
     { name: "hashtag2", id: "1" },
     { name: "hashtag3", id: "2" },
   ];
+  const onSubmit = handleSubmit((data) => {
+    toast({
+      title: "Submitted form",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+    console.log(data);
+  });
   const description = [
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos eum fugit ex sed saepe quo consectetur nostrum illo autem recusandae.",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat alias facere itaque natus repellendus debitis voluptas facilis maiores quia rerum.",
@@ -22,102 +98,127 @@ export default function OfferPage() {
   ];
 
   return (
-    <div className="flex flex-col items-start gap-20 px-16 py-8">
-      {/* HEADER */}
-      <div className="flex items-start gap-10">
-        <img
-          src="https://i.pinimg.com/736x/dc/e1/8e/dce18e21ab55156563e17affb71314fc.jpg"
-          alt="avatar"
-          className="size-64 rounded-full"
-        />
-
-        <div className="mt-4 flex flex-col items-start justify-center gap-4">
-          <div className="flex items-end justify-center gap-12">
-            {/* Offer create */}
-            <h1 className="break-all text-4xl font-semibold uppercase text-blue-950 outline-none">
-              MICHAŁ MATCZAK
-            </h1>
-            <h3 className="text-gra text-2xl font-bold capitalize text-black/40">
-              {/* Warszawa */}
-              Location
-            </h3>
-          </div>
-          <div className="flex items-center justify-center gap-4">
-            <StarRating currentRating={3.4} />
-            <Icon name="badge-check" className="size-12" />
-          </div>
-          <div className="flex items-start justify-center gap-4">
-            {!tags && (
-              <div className="animate-pulse rounded-md bg-muted-foreground px-40 py-6" />
-            )}
-            {tags?.map((tag, index) => <Tag label={tag.name} key={index} />)}
-          </div>
-          {/* TODO quick placeholder - get someone to design this */}
-          <div className="flex items-center gap-3 rounded-full border bg-primary stroke-primary-foreground px-4 py-2 font-semibold text-primary-foreground">
-            <Icon name="wallet" className="size-8" />
-            <input
-              type="text"
-              min={0}
-              max={999999999}
-              step={1}
-              className="w-[5.8rem] bg-inherit text-right outline-none"
-              placeholder="XXXXXXXX"
-
-              // TODO - figure this out
-              // onKeyDown={(event) => {
-              //   if (
-              //     !/\d+/.test(event.key) &&
-              //     event.key !== "Backspace" &&
-              //     event.key !== "Delete" &&
-              //     event.key !== "ArrowLeft" &&
-              //     event.key !== "ArrowRight" &&
-              //     event.key !== "Enter" &&
-              //     event.key !== "Home" &&
-              //     event.key !== "End"
-              //   ) {
-              //     event.preventDefault();
-              //   }
-              // }}
-              // onChange={(event) => {
-              //   const val = event.target.value;
-              //   const numVal = Number(val);
-              //   if (numVal > 999999999 && !isNaN(numVal)) {
-              //     event.target.value = val.slice(0, -1);
-              //   }
-              // }}
+    <>
+      <form onSubmit={onSubmit}>
+        <div className="flex flex-col items-start gap-20 px-16 py-8">
+          {/* HEADER */}
+          <div className="flex items-start gap-10">
+            <img
+              src="https://i.pinimg.com/736x/dc/e1/8e/dce18e21ab55156563e17affb71314fc.jpg"
+              alt="avatar"
+              className="size-64 rounded-full"
             />
-            zł
+
+            <div className="mt-4 flex flex-col items-start justify-center gap-4">
+              <div className="flex items-end justify-center gap-12">
+                {/* Offer create */}
+                <h1 className="break-all text-4xl font-semibold uppercase text-blue-950 outline-none">
+                  MICHAŁ MATCZAK
+                </h1>
+                {/* TODO placeholder until location stuff is figured out */}
+                <Select>
+                  <SelectTrigger className="w-fit text-2xl font-bold capitalize text-black/40">
+                    <SelectValue placeholder="Lokalizacja" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup className="text-2xl font-bold capitalize text-black/40">
+                      <SelectItem value="1">Lokalizacja 1</SelectItem>
+                      <SelectItem value="2">Lokalizacja 2</SelectItem>
+                      <SelectItem value="3">Lokalizacja 3</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex items-center justify-center gap-4">
+                {fields?.map((tag, index) => (
+                  <div
+                    className="pointer-events-none flex h-10 items-center gap-2 rounded-lg bg-purple-400 px-4 font-normal text-white transition-colors hover:bg-destructive"
+                    key={index}
+                  >
+                    <input
+                      {...register(`tags.${index}.name`)}
+                      className="hidden bg-inherit outline-none"
+                    />
+                    {tag.name}
+                    <Icon
+                      name="plus"
+                      className="pointer-events-auto size-5 rotate-45 cursor-pointer"
+                      onClick={() => remove(index)}
+                    />
+                  </div>
+                ))}
+                <Popover open={tagOpen} onOpenChange={setTagOpen}>
+                  <PopoverTrigger className="flex items-center rounded-full border-2 p-1 hover:bg-secondary">
+                    <Icon name="plus" className="size-6" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit p-0">
+                    {/* TODO add search */}
+                    <ScrollArea className="max-h-[20rem] rounded-lg">
+                      <div className="flex flex-col gap-1 p-2">
+                        {tags?.map((tag, index) => (
+                          <div
+                            key={index}
+                            className=" cursor-pointer divide-y divide-solid divide-black rounded-lg bg-purple-400 p-3 font-semibold text-white transition-colors hover:bg-purple-300"
+                            onClick={() => {
+                              append(tag);
+                              setTagOpen(false);
+                            }}
+                          >
+                            {tag.name}
+                          </div>
+                        ))}
+                      </div>
+                    </ScrollArea>
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <p className="text-sm font-semibold text-red-500">
+                {errors.tags?.message}
+              </p>
+              {/* TODO quick placeholder - get someone to design this */}
+              <label className="flex items-center gap-3 rounded-full border bg-primary stroke-primary-foreground px-4 py-2 font-semibold text-primary-foreground">
+                <Icon name="wallet" className="size-8" />
+                <input
+                  {...register("price", {
+                    valueAsNumber: true,
+                  })}
+                  className="w-[5.8rem] bg-inherit text-right outline-none"
+                  placeholder="XXXXXXXX"
+                />
+                zł
+              </label>
+              <p className="text-sm font-semibold text-red-500">
+                {errors.price?.message}
+              </p>
+              <button type="submit">TEST</button>
+            </div>
           </div>
+
+          {/* O MNIE */}
+          <OfferSegment heading="O MNIE">test</OfferSegment>
+
+          {/* CO OFERUJĘ */}
+          <OfferSegment heading="CO OFERUJĘ" info={description} />
+
+          {/* MOJE PORTFOLIO */}
+          <OfferSegment
+            heading="MOJE PORTFOLIO"
+            info={[
+              "https://www.youtube.com/embed/F2YpXC1itEE",
+              "https://www.youtube.com/embed/F2YpXC1itEE",
+            ]}
+            type="video"
+          />
+
+          {/* LINKI */}
+          <OfferSegment
+            heading="LINKI"
+            info={["https://www.youtube.com/embed/F2YpXC1itEE"]}
+            type="link"
+          />
         </div>
-      </div>
-
-      {/* O MNIE */}
-      <OfferSegment heading="O MNIE" info={description} />
-
-      {/* CO OFERUJĘ */}
-      <OfferSegment heading="CO OFERUJĘ" info={description} />
-
-      {/* MOJE PORTFOLIO */}
-      <OfferSegment
-        heading="MOJE PORTFOLIO"
-        info={[
-          "https://www.youtube.com/embed/F2YpXC1itEE",
-          "https://www.youtube.com/embed/F2YpXC1itEE",
-        ]}
-        type="video"
-      />
-
-      {/* LINKI */}
-      <OfferSegment
-        heading="LINKI"
-        info={["https://www.youtube.com/embed/F2YpXC1itEE"]}
-        type="link"
-      />
-
-      {/* OPINIE */}
-      <OfferSegment heading="OPINIE">
-        TUTAJ IDĄ KOMENTARZE I OPINIE
-      </OfferSegment>
-    </div>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 }
--- src/components/ui/SvgSymbols/SvgSymbols.tsx
+++ src/components/ui/SvgSymbols/SvgSymbols.tsx
@@ -38,6 +38,10 @@ const SvgSymbols = (
       <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1" />
       <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4" />
     </symbol>
+    <symbol id="plus">
+      <path d="M5 12h14" />
+      <path d="M12 5v14" />
+    </symbol>
   </svg>
 );
 

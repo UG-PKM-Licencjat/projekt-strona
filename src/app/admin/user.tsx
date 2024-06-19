fd1e69125f310b4c315e96d4f6f52e032f027d5c
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-19 22:23:30 +0200

    Fix build issues and add vercel drizzle adapter

--- src/app/admin/user.tsx
+++ src/app/admin/user.tsx
@@ -3,39 +3,39 @@ import { Button } from '~/components/ui/Button/Button';
 import Image from 'next/image';
 
 export async function User() {
-  const session = await auth();
-  const user = session?.user;
+  // const session = await auth();
+  // const user = session?.user;
 
-  if (!user) {
-    return (
-      <form
-        action={async () => {
-          'use server';
-          await signIn('github');
-        }}
-      >
-        <Button variant="outline">Sign In</Button>
-      </form>
-    );
-  }
+  // if (!user) {
+  //   return (
+  //     <form
+  //       action={async () => {
+  //         'use server';
+  //         await signIn('github');
+  //       }}
+  //     >
+  //       <Button variant="outline">Sign In</Button>
+  //     </form>
+  //   );
+  // }
 
-  return (
-    <div className="flex items-center gap-4">
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <Button variant="outline">Sign Out</Button>
-      </form>
-      <Image
-        className="h-8 w-8 rounded-full"
-        src={user.image!}
-        height={32}
-        width={32}
-        alt={`${user.name} avatar`}
-      />
-    </div>
-  );
+  // return (
+  //   <div className="flex items-center gap-4">
+  //     <form
+  //       action={async () => {
+  //         'use server';
+  //         await signOut();
+  //       }}
+  //     >
+  //       <Button variant="outline">Sign Out</Button>
+  //     </form>
+  //     <Image
+  //       className="h-8 w-8 rounded-full"
+  //       src={user.image!}
+  //       height={32}
+  //       width={32}
+  //       alt={`${user.name} avatar`}
+  //     />
+  //   </div>
+  // );
 }

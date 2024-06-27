893a2a47131744f5e3ab07e65c9523a3c70b2487
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-27 13:39:50 +0200

    Replace env var with correct one

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -6,12 +6,11 @@ import * as schema from "./schema";
 
 export const db = drizzle(sql, { schema });
 
-
 // Universal Database URL version
 
 // import { drizzle } from "drizzle-orm/postgres-js";
 // import postgres from "postgres";
 // import { env } from "~/env.js";
 
-// export const postgresClient = postgres(env.DATABASE_URL);
+// export const postgresClient = postgres(env.POSTGRES_URL);
 // export const db = drizzleJS(postgresClient, { schema });

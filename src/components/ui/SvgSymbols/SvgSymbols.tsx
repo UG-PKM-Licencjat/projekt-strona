ad16517acac07eac19c8bd69baf165dcf03f95b4
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-30 19:48:14 +0200

    Add trash icon

--- src/components/ui/SvgSymbols/SvgSymbols.tsx
+++ src/components/ui/SvgSymbols/SvgSymbols.tsx
@@ -47,9 +47,16 @@ const SvgSymbols = (
       <polyline points="17 8 12 3 7 8" />
       <line x1="12" x2="12" y1="3" y2="15" />
     </symbol>
-    <svg id="message-square">
+    <symbol id="message-square">
       <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
-    </svg>
+    </symbol>
+    <symbol id="trash">
+      <path d="M3 6h18" />
+      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+      <line x1="10" x2="10" y1="11" y2="17" />
+      <line x1="14" x2="14" y1="11" y2="17" />
+    </symbol>
   </svg>
 );
 

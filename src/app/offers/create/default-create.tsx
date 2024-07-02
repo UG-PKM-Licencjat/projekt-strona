ab0d245ef4edc9828d85ea93710bb4a5d6c5fb55
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-02 21:02:04 +0200

    Extract components

--- src/app/offers/create/default-create.tsx
+++ src/app/offers/create/default-create.tsx
@@ -18,25 +18,11 @@ import {
 } from "~/components/ui/popover";
 import { ScrollArea } from "~/components/ui/scroll-area";
 import { useToast } from "~/components/ui/use-toast";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  DialogTitle,
-} from "~/components/ui/dialog";
-
-import { motion, AnimatePresence } from "framer-motion";
-
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { type FormData } from "./schema";
-
 import { SegmentField } from "./segment-field";
-import { UploadDropzone } from "~/components/uploadthing";
-import { trpc } from "~/app/_trpc/client";
 import Image from "next/image";
-
-import { type ClientUploadedFileData } from "uploadthing/types";
-import { Button } from "~/components/ui/Button/Button";
+import { FileUpload } from "./file-upload";
 
 export function DefaultCreateOfferPage() {
   const { toast } = useToast();
@@ -48,68 +34,6 @@ export function DefaultCreateOfferPage() {
   } = useFormContext<FormData>();
 
   const [tagOpen, setTagOpen] = React.useState(false);
-  const [files, setfiles] = React.useState<ClientUploadedFileData<null>[]>([]);
-  const [dialogOpen, setDialogOpen] = React.useState(false);
-  const [fileIsDeleting, setFileIsDeleting] = React.useState<string[]>([]);
-  const [uploading, setUploading] = React.useState(false);
-
-  const deleteFilesMutation = trpc.deleteFiles.useMutation();
-
-  const deleteFile = async (fileKey: string) => {
-    setFileIsDeleting([...fileIsDeleting, fileKey]);
-    deleteFilesMutation
-      .mutateAsync({ fileKeys: fileKey })
-      .then((success) => {
-        if (success) {
-          setfiles((prevfiles) =>
-            prevfiles.filter((file) => file.key !== fileKey),
-          );
-          setFileIsDeleting((prevFileIsDeleting) =>
-            prevFileIsDeleting.filter((file) => file !== fileKey),
-          );
-        } else {
-          toast({
-            title: "Wystąpił błąd",
-            description: "Nie udało się usunąć pliku",
-            variant: "destructive",
-          });
-        }
-      })
-      .catch((err) => {
-        console.log("Error deleting file", err);
-        toast({
-          title: "Wystąpił błąd",
-          description: "Nie udało się usunąć pliku",
-          variant: "destructive",
-        });
-      });
-  };
-
-  const deleteAll = () => {
-    setFileIsDeleting(files.map((file) => file.key));
-    deleteFilesMutation
-      .mutateAsync({ fileKeys: files.map((file) => file.key) })
-      .then((success) => {
-        if (success) {
-          setfiles([]);
-          setFileIsDeleting([]);
-        } else {
-          toast({
-            title: "Wystąpił błąd",
-            description: "Nie udało się usunąć plików",
-            variant: "destructive",
-          });
-        }
-      })
-      .catch((err) => {
-        console.log("Error deleting files", err);
-        toast({
-          title: "Wystąpił błąd",
-          description: "Nie udało się usunąć plików",
-          variant: "destructive",
-        });
-      });
-  };
 
   const { fields, append, remove } = useFieldArray({
     control,
@@ -272,127 +196,7 @@ export function DefaultCreateOfferPage() {
 
           {/* MOJE PORTFOLIO */}
           <OfferSegment heading="MOJE PORTFOLIO">
-            <div className="flex items-center justify-center gap-4">
-              <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-                <DialogTrigger asChild>
-                  <Button className="flex items-center justify-center gap-2">
-                    <Icon name="upload" className="size-5" />
-                    Prześlij pliki
-                  </Button>
-                </DialogTrigger>
-                <DialogContent
-                  onInteractOutside={(e) => {
-                    if (uploading) {
-                      e.preventDefault();
-                    }
-                  }}
-                  disableClose={uploading}
-                >
-                  <DialogTitle>Prześlij pliki</DialogTitle>
-                  {/* TODO customize style and text */}
-                  <UploadDropzone
-                    endpoint="fileUploader"
-                    onBeforeUploadBegin={(files) => {
-                      setUploading(true);
-                      return files;
-                    }}
-                    onUploadAborted={() => setUploading(false)}
-                    onClientUploadComplete={(success) => {
-                      // Do something with the successponse
-                      console.log("Files: ", success);
-                      success ? setfiles([...files, ...success]) : null;
-                      setDialogOpen(false);
-                      setUploading(false);
-                      // alert("Upload Completed");
-                    }}
-                    onUploadError={(error: Error) => {
-                      // Do something with the error.
-                      alert(`ERROR! ${error.message}`);
-
-                      setUploading(false);
-                    }}
-                  />
-                </DialogContent>
-              </Dialog>
-              {files.length > 0 && (
-                <Button
-                  onClick={deleteAll}
-                  className="flex w-fit items-center justify-center gap-2 px-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-60"
-                  variant="error"
-                  type="button"
-                  disabled={fileIsDeleting.length > 0}
-                >
-                  {fileIsDeleting.length > 0 ? (
-                    <Icon name="spinner" className="size-5 animate-spin" />
-                  ) : (
-                    <Icon name="trash" className="size-5" />
-                  )}
-                  Usuń wszystkie
-                </Button>
-              )}
-            </div>
-
-            <motion.div className="flex flex-wrap gap-4">
-              <AnimatePresence initial={false}>
-                {files.map((file) => (
-                  <motion.div
-                    key={file.key}
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.5 }}
-                    className="group relative flex p-2"
-                    layout
-                  >
-                    <div className="relative flex h-44 overflow-hidden rounded-lg border-2">
-                      {file.type.startsWith("image") ? (
-                        <Image
-                          src={file.url}
-                          alt={file.name}
-                          width={150}
-                          height={150}
-                          style={{ width: "100%", height: "auto" }}
-                          sizes="20vw"
-                        />
-                      ) : file.type.startsWith("video") ? (
-                        <video controls>
-                          <source src={file.url} type={file.type} />
-                        </video>
-                      ) : file.type.startsWith("audio") ? (
-                        // PLACEHOLDER (?)
-                        <div className="flex flex-col items-center justify-center gap-2">
-                          <p className="text-center text-sm font-semibold">
-                            {file.name}
-                          </p>
-                          <audio controls>
-                            <source src={file.url} type={file.type} />
-                          </audio>
-                        </div>
-                      ) : null}
-                      {fileIsDeleting.includes(file.key) ? (
-                        <div className="absolute inset-0 flex items-center justify-center bg-black/50 text-white">
-                          <Icon
-                            name="spinner"
-                            className="size-10 animate-spin"
-                          />
-                        </div>
-                      ) : null}
-                    </div>
-                    <div
-                      className="absolute right-0 top-0 rotate-180 scale-0 cursor-pointer rounded-full bg-destructive p-1 transition-all duration-200 hover:bg-red-600 group-hover:rotate-0 group-hover:scale-100"
-                      onClick={() => {
-                        void deleteFile(file.key);
-                      }}
-                      hidden={fileIsDeleting.includes(file.key)}
-                    >
-                      <Icon
-                        name="plus"
-                        className="size-4 rotate-45 stroke-destructive-foreground"
-                      />
-                    </div>
-                  </motion.div>
-                ))}
-              </AnimatePresence>
-            </motion.div>
+            <FileUpload />
           </OfferSegment>
 
           {/* LINKI */}
---
+++ src/app/offers/create/file-preview.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { type ClientUploadedFileData } from "uploadthing/types";
+import Image from "next/image";
+import { Icon } from "~/components/ui/Icon/Icon";
+
+export function FilePreview({
+  file,
+  fileIsDeleting,
+  deleteFile,
+}: {
+  file: ClientUploadedFileData<null>;
+  fileIsDeleting: string[];
+  deleteFile: (fileKey: string) => void;
+}) {
+  return (
+    <>
+      <div className="relative flex h-44 overflow-hidden rounded-lg border-2">
+        {file.type.startsWith("image") ? (
+          <Image
+            src={file.url}
+            alt={file.name}
+            width={150}
+            height={150}
+            style={{ width: "100%", height: "auto" }}
+            sizes="20vw"
+          />
+        ) : file.type.startsWith("video") ? (
+          <video controls>
+            <source src={file.url} type={file.type} />
+          </video>
+        ) : file.type.startsWith("audio") ? (
+          // PLACEHOLDER (?)
+          <div className="flex flex-col items-center justify-center gap-2">
+            <p className="text-center text-sm font-semibold">{file.name}</p>
+            <audio controls>
+              <source src={file.url} type={file.type} />
+            </audio>
+          </div>
+        ) : null}
+        {fileIsDeleting.includes(file.key) ? (
+          <div className="absolute inset-0 flex items-center justify-center bg-black/50 text-white">
+            <Icon name="spinner" className="size-10 animate-spin" />
+          </div>
+        ) : null}
+      </div>
+      <div
+        className="absolute right-0 top-0 rotate-180 scale-0 cursor-pointer rounded-full bg-destructive p-1 transition-all duration-200 hover:bg-red-600 group-hover:rotate-0 group-hover:scale-100"
+        onClick={() => {
+          void deleteFile(file.key);
+        }}
+        hidden={fileIsDeleting.includes(file.key)}
+      >
+        <Icon
+          name="plus"
+          className="size-4 rotate-45 stroke-destructive-foreground"
+        />
+      </div>
+    </>
+  );
+}

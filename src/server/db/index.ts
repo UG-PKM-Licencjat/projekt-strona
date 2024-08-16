4fc9a52d7b52e330e61591dd6ca91534290c7756
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-16 14:35:29 +0200

    Rewrite db connection to change adapters based on NODE_ENV

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,23 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { sql } from "@vercel/postgres";
+import {
+  drizzle as drizzleVercel,
+  type VercelPgDatabase,
+} from "drizzle-orm/vercel-postgres";
+import {
+  drizzle as drizzleJS,
+  type PostgresJsDatabase,
+} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-export const postgresClient = postgres(env.POSTGRES_URL);
-export const db = drizzle(postgresClient, { schema });
+export let db:
+  | VercelPgDatabase<typeof schema>
+  | PostgresJsDatabase<typeof schema>;
+
+if (env.NODE_ENV === "production") {
+  db = drizzleVercel(sql, { schema });
+} else {
+  const postgresClient = postgres(env.POSTGRES_URL);
+  db = drizzleJS(postgresClient, { schema });
+}

30d88e488c9345aebb1c5b1c0d9c4ecaab15d981
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-16 14:18:26 +0200

    Reverse merge error

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -47,9 +47,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      >
-        {children}
-      </button>
+      />
     );
   },
 );

85486de62ec38fe5e76582cc9bc1ea68d182214f
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-16 14:20:26 +0200

    Change db connection adapter

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,16 +1,7 @@
-// Vercel postgres version
-
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { env } from "~/env.js";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
-
-// Universal Database URL version
-
-// import { drizzle } from "drizzle-orm/postgres-js";
-// import postgres from "postgres";
-// import { env } from "~/env.js";
-
-// export const postgresClient = postgres(env.POSTGRES_URL);
-// export const db = drizzleJS(postgresClient, { schema });
+export const postgresClient = postgres(env.POSTGRES_URL);
+export const db = drizzle(postgresClient, { schema });

c09f46d4c406b479dc2b18c23d52bfc6dbefc9a3
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-16 15:30:30 +0200

    Test something

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -11,7 +11,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: "https://localhost:3000/api/trpc",
         }),
       ],
     }),

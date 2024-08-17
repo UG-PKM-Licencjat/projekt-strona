3fa40a171646b3e28399bad980ea14ed9e86419e
Magda <111302737+magda1231@users.noreply.github.com>
2024-08-17 23:45:31 +0200

    registration pages layout changed

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,13 +1,31 @@
+import { Icon } from "~/components/ui/Icon/Icon";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-neo-castleton box-border flex h-screen items-center justify-center">
-      <div className="bg-neo-gray rounded-xlg flex  h-4/5 w-4/5 rounded-xl ">
-        {children}
+    <>
+      <div className="bg-neo-castleton  box-border h-full ">
+        <div className=" flex  w-screen flex-row items-center justify-between font-bold sm:mb-5">
+          <h1 className="ml-10 text-left  text-2xl text-primary">Bebooops</h1>
+          <Icon
+            className="mr-10  block sm:hidden"
+            name="menu-bar"
+            width={30}
+            height={30}
+          />
+        </div>
+
+        <div className="flex  w-screen items-center justify-center sm:h-5/6 ">
+          <div className="md:bg-neo-gray rounded-xlg flex h-full  w-full justify-center overflow-auto rounded-xl  bg-none   sm:w-9/12">
+            <div className="  h-full w-full justify-between  rounded-lg px-14 py-10 align-middle ">
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }

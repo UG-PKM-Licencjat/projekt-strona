910de4ca024468d137d4b293c07c17e50095c145
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-19 01:55:55 +0200

    Add db caching in dev environment

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -8,8 +8,20 @@ import {
   type PostgresJsDatabase,
 } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { env } from "~/env.js";
+
+import { env } from "~/env";
 import * as schema from "./schema";
+
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: postgres.Sql | undefined;
+};
+
+const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export let db:
   | VercelPgDatabase<typeof schema>
@@ -18,6 +30,5 @@ export let db:
 if (env.NODE_ENV === "production") {
   db = drizzleVercel(sql, { schema });
 } else {
-  const postgresClient = postgres(env.POSTGRES_URL);
-  db = drizzleJS(postgresClient, { schema });
+  db = drizzleJS(conn, { schema });
 }

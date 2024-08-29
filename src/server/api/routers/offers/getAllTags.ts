ff0fb8c2582204e1187583eb96d0b5bf7ca6e5c3
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-29 20:04:33 +0200

    Add generating new tags on getAllTags query when there are none in db

--- src/server/api/routers/offers/getAllTags.ts
+++ src/server/api/routers/offers/getAllTags.ts
@@ -4,6 +4,19 @@ import { procedure } from "~/server/api/trpc";
 
 const getAllTagsProcedure = procedure.query(async () => {
   const allTags = await db.select().from(tags);
+  // TODO delete this before deploying
+  if (!allTags || allTags.length === 0) {
+    console.log("No tags found, adding some");
+    const newTags = await db
+      .insert(tags)
+      .values(
+        Array.from({ length: 10 }, (_, i) => ({
+          name: `hashtag${i + 1}`,
+        })),
+      )
+      .returning();
+    return newTags;
+  }
   return allTags;
 });
 

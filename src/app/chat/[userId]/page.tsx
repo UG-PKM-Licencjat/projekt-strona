518c5f36bc03050597c4bdfd49dd207feab0ec52
piotrselak <piotr.selak.it@gmail.com>
2024-09-07 20:20:37 +0200

    Removed trash code

--- src/app/chat/[userId]/page.tsx
+++ src/app/chat/[userId]/page.tsx
@@ -3,74 +3,13 @@
 import { Send } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useMemo, useState } from "react";
-import ConversationWindow, {
+import {
   type Message,
 } from "src/components/chat/ConversationWindow/ConversationWindow";
 import { Button } from "src/components/ui/Button/Button";
 import { Input } from "src/components/ui/Input/Input";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
-// export default function Conversation({
-//   params,
-// }: {
-//   params: { userId: string };
-// }) {
-//   const { userId } = params;
-//   const [message, setMessage] = useState("");
-//   const [messages, setMessages] = useState<Array<Message>>([]);
-//   const { data: session } = useSession();
-
-//   void useMemo(async () => {
-//     console.log("Loading data for", session?.user.id);
-//     const response = await fetch(
-//       `https://chat-swxn.onrender.com/messages?userA=${session?.user.id}&userB=${userId}`,
-//     );
-
-//     // TODO: Validate schema?
-//     const initialMessages = (await response.json()) as {
-//       messages: Array<Message>;
-//     };
-
-//     setMessages(initialMessages.messages);
-//   }, [session?.user.id, userId]);
-
-//   async function handleSubmit() {
-//     const response = await fetch("https://chat-swxn.onrender.com/messages", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         message: message,
-//         from: session?.user.id,
-//         to: userId,
-//       }),
-//     });
-//     // Todo also validate
-//     const newMessage = (await response.json()) as { message: Message };
-//     // TODO when status will be known change it to exact one
-//     if (response.status < 300) {
-//       setMessages([...messages, newMessage.message]);
-//     }
-//     setMessage("");
-//   }
-
-//   return (
-//     <div>
-//       <ConversationWindow data={messages} />
-//       <Input
-//         value={message}
-//         onChange={(event) => {
-//           setMessage(event.target.value);
-//         }}
-//         placeholder="Hej! Czy dasz radę zagrać na..."
-//       />
-//       <Button type="submit" onClick={handleSubmit}>
-//         Send
-//       </Button>
-//     </div>
-//   );
-// }
 
 export default function Conversation({
   params,

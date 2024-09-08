1932241dff3c6d30377da1fedad87bf741e4c514
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-08 22:41:36 +0200

    Replace UserMenu popover with custom shadcn one, add Icons

--- src/components/Navbar/UserMenu.tsx
+++ src/components/Navbar/UserMenu.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { type Session } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
@@ -68,48 +67,6 @@ const UserMenu = ({ session }: UserMenuProps) => {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-
-    // <div className="relative">
-    //   <button
-    //     onClick={toggleMenu}
-    //     className="flex items-center focus:outline-none"
-    //   >
-    //     <User />
-    //   </button>
-
-    //   {isMenuOpen && (
-    //     <div className="absolute right-0 z-50 mt-2 w-48 rounded-md bg-white shadow-lg">
-    //       <ul className="py-2">
-    //         <li>
-    //           <Link
-    //             href="/profile"
-    //             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-    //           >
-    //             Profil
-    //           </Link>
-    //         </li>
-    //         {session.user?.admin && (
-    //           <li>
-    //             <Link
-    //               href="/admin"
-    //               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-    //             >
-    //               Panel Administratora
-    //             </Link>
-    //           </li>
-    //         )}
-    //         <li>
-    //           <button
-    //             onClick={() => signOut({ callbackUrl: "/" })}
-    //             className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-    //           >
-    //             Wyloguj
-    //           </button>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   )}
-    // </div>
   );
 };
 

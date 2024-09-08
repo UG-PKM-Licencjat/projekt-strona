f6d69fdd52a56306e1c7de4f968b982cf5b4c93b
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-08 20:57:20 +0200

    Run prettier

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,53 +7,56 @@ import { type Session } from "next-auth";
 import UserMenu from "./UserMenu";
 import { usePathname } from "next/navigation";
 
-const filterList: Array<string> = []
+const filterList: Array<string> = [];
 
 export const Navbar = () => {
   const { data: session } = useSession();
-  const pathname = usePathname()
+  const pathname = usePathname();
 
-  if (filterList.includes(pathname))
-    return <></> 
+  if (filterList.includes(pathname)) return <></>;
 
-  return <nav className="sticky top-0 z-50 bg-neo-castleton text-white">
-    <div className="container flex flex-col p-4">
-      <div className="flex flex-row justify-between items-center">
-        <Link href="/">
-          <Icon
-            name="logo"
-            className="h-[50px] max-md:h-7"
-            viewBox="0 0 151 44"
-          />
-        </Link>
-        {buildNavbarElements(session)}
+  return (
+    <nav className="sticky top-0 z-50 bg-neo-castleton text-white">
+      <div className="container flex flex-col p-4">
+        <div className="flex flex-row items-center justify-between">
+          <Link href="/">
+            <Icon
+              name="logo"
+              className="h-[50px] max-md:h-7"
+              viewBox="0 0 151 44"
+            />
+          </Link>
+          {buildNavbarElements(session)}
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  );
 };
 
 const buildNavbarElements = (session: Session | null): JSX.Element => (
   <div className="flex gap-3">
     <Link href="/offers">Oferty</Link>
-    {!session && <Button
-      className="rounded-full bg-white px-4 py-2 text-green-900 shadow-md"
-      variant={null}
-      size="sm"
-      onClick={() =>
-        signIn("google", {
-          callbackUrl: "http://localhost:3000",
-        })
-      }
-    >
-      Sign in with Google
-    </Button>}
-    {session &&
+    {!session && (
+      <Button
+        className="rounded-full bg-white px-4 py-2 text-green-900 shadow-md"
+        variant={null}
+        size="sm"
+        onClick={() =>
+          signIn("google", {
+            callbackUrl: "http://localhost:3000",
+          })
+        }
+      >
+        Sign in with Google
+      </Button>
+    )}
+    {session && (
       <>
         <Link href="/chat">Wiadomości</Link>
         <div className="relative">
           <UserMenu session={session} />
         </div>
-      </> 
-    }
+      </>
+    )}
   </div>
-)
+);
--- src/components/Navbar/UserMenu.tsx
+++ src/components/Navbar/UserMenu.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { type Session } from 'next-auth';
-import { signOut } from 'next-auth/react';
-import Link from 'next/link';
-import { User } from 'lucide-react';
+import { useState } from "react";
+import { type Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { User } from "lucide-react";
 
 interface UserMenuProps {
   session: Session;
@@ -15,29 +15,38 @@ const UserMenu = ({ session }: UserMenuProps) => {
 
   return (
     <div className="relative">
-      <button onClick={toggleMenu} className="flex items-center focus:outline-none">
-        <User/>
+      <button
+        onClick={toggleMenu}
+        className="flex items-center focus:outline-none"
+      >
+        <User />
       </button>
 
       {isMenuOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50">
+        <div className="absolute right-0 z-50 mt-2 w-48 rounded-md bg-white shadow-lg">
           <ul className="py-2">
             <li>
-              <Link href="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              <Link
+                href="/profile"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              >
                 Profil
               </Link>
             </li>
             {session.user?.admin && (
               <li>
-                <Link href="/admin" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                <Link
+                  href="/admin"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
                   Panel Administratora
                 </Link>
               </li>
             )}
             <li>
               <button
-                onClick={() => signOut({ callbackUrl: '/' })}
-                className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                onClick={() => signOut({ callbackUrl: "/" })}
+                className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
               >
                 Wyloguj
               </button>

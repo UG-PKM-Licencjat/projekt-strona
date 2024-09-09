6d6cb1d2a15d0ae2b86afa3c0b53145428e5caff
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-09 16:01:24 +0200

    Add custom Navbar link with current path check for styling

---
+++ src/components/Navbar/NavbarLink.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link, { type LinkProps } from "next/link";
+import { Icon } from "~/components/ui/Icon/Icon";
+import type { IconType } from "~/components/ui/SvgSymbols/SvgSymbols";
+import { usePathname } from "next/navigation";
+import { cn } from "~/lib/utils";
+
+export const NavbarLink = ({
+  children,
+  icon,
+  ...props
+}: {
+  children: React.ReactNode;
+  icon?: IconType;
+} & LinkProps) => {
+  const pathname = usePathname();
+
+  return (
+    <Link {...props}>
+      <div
+        className={cn(
+          "flex items-center gap-2 text-lg text-neo-gray-hover transition-colors hover:text-neo-sage",
+          pathname === props.href && "text-neo-sage",
+        )}
+      >
+        {!!icon && <Icon name={icon} className="size-6" />}
+        {children}
+      </div>
+    </Link>
+  );
+};

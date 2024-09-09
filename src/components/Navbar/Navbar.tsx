21837f41cb435d2c3d755a2e178a075cd1d4761a
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-09 16:03:03 +0200

    Finish main navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import Link from "next/link";
 import { Icon } from "../ui/Icon/Icon";
 import { signIn, useSession } from "next-auth/react";
-import { type Session } from "next-auth";
 import UserMenu from "./UserMenu";
 import { usePathname } from "next/navigation";
+import SkeletonCard from "~/components/ui/SkeletonCard/SkeletonCard";
+import { NavbarLink } from "./NavbarLink";
 
 const filterList: Array<string> = [];
 
@@ -15,43 +16,45 @@ export const Navbar = () => {
   if (filterList.includes(pathname)) return <></>;
 
   return (
-    <nav className="sticky top-0 z-50 bg-neo-castleton text-white">
-      <div className="container flex flex-col p-4">
+    <nav className="fixed z-50 w-full bg-neo-castleton max-sm:bottom-10 sm:sticky sm:top-0">
+      <div className="container hidden flex-col p-4 text-white sm:flex">
         <div className="flex flex-row items-center justify-between">
           <Link href="/">
             <Icon
               name="logo"
-              className="h-[50px] max-md:h-7"
+              className="hidden sm:flex sm:h-[50px]"
               viewBox="0 0 151 44"
             />
           </Link>
-          {status}
-          {buildNavbarElements(session)}
+          <div className="flex items-center gap-6">
+            <NavbarLink href="/offers" icon="magnifier">
+              <span className="hidden sm:flex">Szukaj</span>
+            </NavbarLink>
+            {session && (
+              <>
+                <NavbarLink href="/chat" icon="message-square">
+                  Chat
+                </NavbarLink>
+                <UserMenu session={session} />
+              </>
+            )}
+            {!session && status === "loading" && (
+              <>
+                <SkeletonCard className="h-5 w-44 rounded" />
+              </>
+            )}
+            {!session && status === "unauthenticated" && (
+              <button
+                className="flex items-center gap-2 rounded-full bg-neo-gray px-4 py-2.5 font-semibold text-neo-castleton shadow-md transition-colors hover:bg-neo-gray-hover"
+                onClick={() => signIn("google")}
+              >
+                <Icon name="google" className="size-6 stroke-none" />
+                Zaloguj się przez Google
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </nav>
   );
 };
-
-const buildNavbarElements = (session: Session | null): JSX.Element => (
-  <div className="flex items-center gap-3">
-    <Link href="/offers">Oferty</Link>
-    {!session && (
-      <button
-        className="flex items-center gap-2 rounded-full bg-neo-gray px-4 py-2.5 font-semibold text-neo-castleton shadow-md transition-colors hover:bg-neo-gray-hover"
-        onClick={() => signIn("google")}
-      >
-        <Icon name="google" className="size-6 stroke-none" />
-        Zaloguj się przez Google
-      </button>
-    )}
-    {session && (
-      <>
-        <Link href="/chat">Wiadomości</Link>
-        <div className="relative">
-          <UserMenu session={session} />
-        </div>
-      </>
-    )}
-  </div>
-);

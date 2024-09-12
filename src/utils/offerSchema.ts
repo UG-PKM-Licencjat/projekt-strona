7afbbb8933db3568ef3cc0e74bfe77155720afad
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-09-12 20:10:23 +0200

    ?

---
+++ src/utils/offerSchema.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+const offerSchema = z
+  .object({
+    price: z.string().regex(/^\d{1,9}(\,\d{1,2})?$/, {
+      message:
+        "Cena powinna być podana w formacie 12345,50 i nie przekraczać 999 999 999,99 zł",
+    }),
+    tags: z
+      .array(z.object({ name: z.string(), id: z.string() }))
+      .nonempty({ message: "Musisz dodać co najmniej jeden tag" }),
+    about: z.array(
+      z.object({
+        text: z.string().min(1, { message: "Pole nie może być puste" }),
+      }),
+    ),
+    skills: z.array(
+      z.object({
+        text: z.string().min(1, { message: "Pole nie może być puste" }),
+      }),
+    ),
+    files: z.array(
+      z.object({
+        url: z.string().url(),
+        type: z.string(),
+      }),
+    ),
+    links: z.array(
+      z.object({
+        text: z.string().url({ message: "Wklej poprawny link" }),
+      }),
+    ),
+  })
+  .required();
+
+type FormData = z.infer<typeof offerSchema>;
+
+export { offerSchema, type FormData };

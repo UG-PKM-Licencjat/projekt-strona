c5aa854a5b63ccdd0e4fe2a519c2af6f14f942ec
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-13 23:20:07 +0200

    Add profile creation route to Navbar filterList

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -21,7 +21,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/Button/Button";
 
-const filterList: Array<string> = [];
+const filterList: Array<string> = ["/profile/create"];
 
 export const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);

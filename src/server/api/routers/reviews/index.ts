da3822e57cf009f8fe38db1420e9bd541ce2ce27
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-09-13 18:13:22 +0200

    delete procedure review

--- src/server/api/routers/reviews/index.ts
+++ src/server/api/routers/reviews/index.ts
@@ -1,9 +1,9 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 
 import { createProcedure } from "./create";
+import { deleteProcedure } from "./delete";
 
-export const RatingsRouter = createTRPCRouter({
-    create: createProcedure,
-    delete:
-    comment:
+export const ReviewsRouter = createTRPCRouter({
+  create: createProcedure,
+  delete: deleteProcedure,
 });

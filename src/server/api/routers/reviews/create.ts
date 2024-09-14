d062a4ec1ecfda2d3685aa84d2b041ffa2e4d16c
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-09-14 18:31:13 +0200

    changed create review procedure so it increments replies count for review its replying to

--- src/server/api/routers/reviews/create.ts
+++ src/server/api/routers/reviews/create.ts
@@ -4,7 +4,7 @@ import { reviews, users, offers } from "~/server/db/schema";
 import { z } from "zod";
 import logEvent, { LogType } from "~/server/log";
 import { TRPCError } from "@trpc/server";
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 
 const COMMENT_LENGTH = 500;
 
@@ -57,31 +57,73 @@ export const createProcedure = procedure
       });
     }
 
-    let values;
     if (replyTo) {
-      values = { offerId, userId, comment, replyTo };
-    } else {
-      values = { offerId, userId, comment };
-    }
-    const result = await db.insert(reviews).values(values);
+      const values = { offerId, userId, comment, replyTo };
+      const { data, error } = await db.transaction(async (tx) => {
+        // add reply as a new review
+        const [returned] = await tx.insert(reviews).values(values).returning();
 
-    if (!result) {
-      logEvent(
-        "Failed to create review",
-        JSON.stringify(result),
-        LogType.ERROR,
-      );
-      return new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Failed to create review",
+        if (!returned) {
+          tx.rollback();
+          return { data: null, error: "Failed to create review (reply)" };
+        }
+
+        const [increment] = await tx
+          .update(reviews)
+          .set({ replies: sql`${reviews.replies} + 1` })
+          .where(eq(reviews.id, replyTo))
+          .returning();
+
+        if (!increment) {
+          tx.rollback();
+          return {
+            data: null,
+            error:
+              "Failed to increment replies count for review you're replying to",
+          };
+        }
+
+        return { data: returned, error: null };
       });
-    }
 
-    logEvent(
-      `User ${userId} added review to offer ${offerId}`,
-      JSON.stringify(result),
-    );
-    return result;
+      if (data) {
+        logEvent(
+          `User ${userId} replied to review ${replyTo} with review ${data?.id}`,
+          JSON.stringify(data),
+        );
+        return data;
+      } else {
+        logEvent(
+          "Failed to create review (reply)",
+          JSON.stringify(error),
+          LogType.ERROR,
+        );
+        return new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error,
+        });
+      }
+    } else {
+      const values = { offerId, userId, comment };
+      const result = await db.insert(reviews).values(values);
+      if (!result) {
+        logEvent(
+          "Failed to create review",
+          JSON.stringify(result),
+          LogType.ERROR,
+        );
+        return new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create review",
+        });
+      }
+
+      logEvent(
+        `User ${userId} added review to offer ${offerId}`,
+        JSON.stringify(result),
+      );
+      return result;
+    }
   });
 
 export default createProcedure;

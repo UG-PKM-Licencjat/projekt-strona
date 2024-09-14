032140d79b1af4a0e5c2e3e618d32828f3ebb5c0
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-14 23:23:26 +0200

    Start work on steps

--- src/app/profile/create/steps/Step1.tsx
+++ src/app/profile/create/steps/Step1.tsx
@@ -1,11 +1,21 @@
 import { useFormContext } from "react-hook-form";
-import { Input } from "~/components/ui/Input/Input";
+import TipTap from "~/components/RichTextEditor/Tiptap";
+import { useState } from "react";
 
 export default function Step1() {
   const { register } = useFormContext();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   return (
-    <div>
-      <Input {...register("name")} placeholder="Imię i nazwisko" />
+    <div className="flex flex-col gap-4">
+      {/* <Input {...register("name")} placeholder="Tytuł" /> */}
+      <div className="rounded-md bg-white drop-shadow-md">
+        <TipTap placeholder="Tytuł" charLimit={100} onChange={setTitle} />
+      </div>
+      <div className="rounded-md bg-white drop-shadow-md">
+        <TipTap placeholder="Opis" charLimit={200} onChange={setDescription} />
+      </div>
+      {/* <Input {...register("name")} placeholder="Opis placeholder - tiptap goes here" /> */}
     </div>
   );
 }
--- src/app/profile/create/steps/index.tsx
+++ src/app/profile/create/steps/index.tsx
@@ -4,36 +4,43 @@ import Step3 from "./Step3";
 import { type IconType } from "~/components/ui/SvgSymbols/SvgSymbols";
 
 export const steps: {
+  nav: string;
   title: string;
   description: string;
   content: React.ReactNode;
   icon: IconType;
 }[] = [
   {
-    title: "Opisz siebie",
-    description: "Lorem ipsum kurwa nie wiem co tu dać xDD",
+    nav: "Tytuł i opis",
+    title: "Tytuł i opis",
+    description:
+      "W tym miejscu uzupełnij tytuł swojej działaności i krótki opis, który pozwoli zrozumieć szukającemu, czym się zajmujesz i co oferujesz. Tytuł powinien jak najlepiej opisywać to czym się zajmujesz, w celu łatwiejszego znalezienia. Ten tytuł i opis są tym co wyświetli się w wynikach wyszukiwarki, więc dobrze zastanów się co chcesz w nich zawrzeć.",
     content: <Step1 />,
     icon: "user",
   },
   {
+    nav: "Galeria",
     title: "Galeria",
     description: "Mama powiedziała mi: „nie ufaj im”",
     content: <Step2 />,
     icon: "user",
   },
   {
+    nav: "Linki",
     title: "Linki",
     description: "Posłuchaj, mama chce dobrze",
     content: <Step3 />,
     icon: "user",
   },
   {
+    nav: "Miasto i cena",
     title: "Miasto i cena",
     description: "Mama powiedziała mi: „nie ufaj”",
     content: <Step3 />,
     icon: "user",
   },
   {
+    nav: "Podsumowanie",
     title: "Podsumowanie",
     description: "Się nie wygłupiaj na miarę ich potrzeb",
     content: <Step3 />,

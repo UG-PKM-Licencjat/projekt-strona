1f2eac5ff697d205301215934a75b2e1fe3dc0c0
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-15 19:02:50 +0200

    Remove unused code, clean up comments

--- src/components/uploadthing/index.ts
+++ src/components/uploadthing/index.ts
@@ -1,14 +1,11 @@
-import {
-  // generateUploadButton,
-  // generateUploadDropzone,
-  generateReactHelpers,
-} from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react";
 
 import type { OurFileRouter } from "~/app/api/uploadthing/core";
-export { PreviewDropzone, type CustomFile, type ExpandedRouteConfig } from "./PreviewDropzone";
+export {
+  PreviewDropzone,
+  type CustomFile,
+  type ExpandedRouteConfig,
+} from "./PreviewDropzone";
 
-// export const UploadButton = generateUploadButton<OurFileRouter>();
-// export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
-
-export const { useUploadThing, uploadFiles, getRouteConfig } =
+export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();

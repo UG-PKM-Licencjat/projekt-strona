b5610879987c9d55e7444fba77576c1958b527df
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-16 21:25:20 +0200

    Modify textarea padding

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-lg bg-neo-gray px-6 py-2 text-lg font-semibold tracking-wider placeholder-neo-dark-gray ring-offset-background drop-shadow-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[80px] w-full rounded-lg bg-neo-gray px-6 py-4 text-lg font-semibold tracking-wider placeholder-neo-dark-gray ring-offset-background drop-shadow-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}

1f53fa7c5dde582e9c3af2430c0ab5ad7756586e
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-16 21:29:10 +0200

    Integrate TipTap into Step2

--- src/app/profile/create/steps/Step2.tsx
+++ src/app/profile/create/steps/Step2.tsx
@@ -1,3 +1,28 @@
+import TipTap from "~/components/RichTextEditor/Tiptap";
+import { useFormContext, useController } from "react-hook-form";
+import type { ArtistFormData } from "~/lib/artistSchema";
+
 export default function Step2() {
-  return <div>Content here</div>;
+  const { control } = useFormContext<ArtistFormData>();
+  const {
+    field: { onChange, value, onBlur },
+    formState: { errors },
+  } = useController({
+    name: "longDescription",
+    control,
+  });
+  return (
+    <div>
+      <TipTap
+        placeholder="Opis"
+        onChange={onChange}
+        onBlur={onBlur}
+        charLimit={1000}
+        value={value}
+      />
+      <div className="h-6 text-base tracking-normal text-neo-pink">
+        {errors.longDescription?.message}
+      </div>
+    </div>
+  );
 }

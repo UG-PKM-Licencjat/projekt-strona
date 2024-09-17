866a31b17f0d4ef2e842f8ca3a747be395926e54
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-17 22:15:23 +0200

    Add dnd/sortable contexts

--- src/app/profile/create/steps/Step4.tsx
+++ src/app/profile/create/steps/Step4.tsx
@@ -4,6 +4,27 @@ import {
   useUploadThing,
 } from "~/components/uploadthing";
 import { useState } from "react";
+import {
+  DndContext,
+  MeasuringStrategy,
+  closestCenter,
+  useSensor,
+  useSensors,
+  type MeasuringConfiguration,
+  DragOverlay,
+  DragStartEvent,
+  DragEndEvent,
+  UniqueIdentifier,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
+
+import { SortableItem } from "~/components/Sortable/SortableItem";
+import { PointerSensor, KeyboardSensor } from "~/components/Sortable/sensors";
 
 export default function Step3() {
   // TODO extract this to parent component, figure out how to pass it down and make it work
@@ -16,17 +37,62 @@ export default function Step3() {
       },
     },
   );
-  return (
-    <div className="flex">
-      <PreviewDropzone
-        files={files}
-        setFiles={setFiles}
-        routeConfig={routeConfig}
-        startUpload={startUpload}
-        isUploading={isUploading}
-        showUploadButton={false}
-        className="w-full"
-      />
-    </div>
+
+  // Useful for onDrag overlays and animations
+  const [isDragging, setIsDragging] = useState(false);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
   );
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+    >
+      <SortableContext
+        items={files.map((file) => file.key)}
+        strategy={rectSortingStrategy}
+      >
+        <div className="flex flex-col gap-2">
+          <PreviewDropzone
+            files={files}
+            setFiles={setFiles}
+            routeConfig={routeConfig}
+            startUpload={startUpload}
+            isUploading={isUploading}
+            showUploadButton={true}
+            className="w-full"
+          />
+        </div>
+      </SortableContext>
+    </DndContext>
+  );
+
+  function handleDragStart(event: DragStartEvent) {
+    setIsDragging(true);
+  }
+
+  function handleDragCancel() {
+    setIsDragging(false);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setFiles((files) => {
+        const oldIndex = files.findIndex((file) => file.key === active?.id);
+        const newIndex = files.findIndex((file) => file.key === over?.id!);
+
+        return arrayMove(files, oldIndex, newIndex);
+      });
+    }
+    setIsDragging(false);
+  }
 }

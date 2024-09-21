3f3724357647689c7d5e836c80ecd7d6f08a9f42
piotrselak <piotr.selak.it@gmail.com>
2024-09-22 01:16:14 +0200

    Really many changes

--- src/app/chat/[userId]/page.tsx
+++ src/app/chat/[userId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { Send } from "lucide-react";
 import { useSession } from "next-auth/react";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { Button } from "src/components/ui/Button/Button";
 import { Input } from "src/components/ui/Input/Input";
 import Message from "src/components/chat/Message/Message";
@@ -28,6 +28,20 @@ export default function Conversation({
     await store.fetchMessagesForUser(session, userId);
   }, []);
 
+  useEffect(() => {
+    const listener = (ev: KeyboardEvent) => {
+      if (ev.code === "Enter" || ev.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Run your function.");
+        ev.preventDefault();
+        void handleSubmit();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   async function handleSubmit() {
     if (!otherProviderId || !session) return;
     setMessage("");
@@ -35,14 +49,13 @@ export default function Conversation({
   }
 
   return (
-    <div className="flex flex-1 flex-col bg-neo-gray-hover md:p-6">
-      <div className="flex-1 overflow-y-auto p-4">
+    <div className="flex max-h-[89vh] flex-1 flex-col overflow-y-hidden bg-neo-gray-hover md:p-6">
+      <div className="flex-1 overflow-y-scroll p-4">
         {(store.conversations[userId] ?? []).map((message, ind) => (
           <Message key={ind} message={message} />
         ))}
       </div>
-
-      <div className="">
+      <div className="bg-neo-gray">
         <div className="flex">
           <Input
             value={message}
@@ -53,12 +66,6 @@ export default function Conversation({
             className="mr-2 flex-1 border-[#005243] bg-white text-[#005243] placeholder-[#4a8573]"
           />
           <Button
-            onKeyDown={(e) => {
-              if (!e.shiftKey) {
-                e.preventDefault();
-                handleSubmit();
-              }
-            }}
             onClick={handleSubmit}
             className="bg-neo-pink text-white transition-colors hover:bg-neo-pink-hover"
           >

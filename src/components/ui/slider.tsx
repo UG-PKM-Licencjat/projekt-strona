ed359a2856c4f5b2164051b5c96e82095b3eec1a
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-21 17:30:42 +0200

    Add data-disabled styles to slider

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -17,10 +17,10 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative mx-1.5 h-5 w-full grow overflow-hidden rounded-full bg-neo-dark-gray">
+    <SliderPrimitive.Track className="relative mx-1.5 h-5 w-full grow overflow-hidden rounded-full bg-neo-dark-gray data-[disabled]:bg-neo-gray-hover">
       <SliderPrimitive.Range className="absolute h-full bg-neo-mantis" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-black bg-white ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-black bg-white ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[disabled]:border-neo-dark-gray data-[disabled]:bg-neo-gray" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;

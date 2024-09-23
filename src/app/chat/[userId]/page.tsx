794d018a67391724692472e43f88005be1202e6d
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-09-23 19:24:17 +0200

    wip

--- src/app/chat/[userId]/page.tsx
+++ src/app/chat/[userId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { Send } from "lucide-react";
 import { useSession } from "next-auth/react";
-import { useMemo, useState, useEffect } from "react";
+import { useMemo, useState, useEffect, useRef } from "react";
 import { Button } from "src/components/ui/Button/Button";
 import { Input } from "src/components/ui/Input/Input";
 import Message from "src/components/chat/Message/Message";
@@ -48,12 +48,33 @@ export default function Conversation({
     await store.sendMessage(message, session, userId, otherProviderId);
   }
 
+  // scroll pagination
+  const observerRef = useRef(null);
+  const observer = new IntersectionObserver(
+    (entries: IntersectionObserverEntry[]) => {
+      if (entries[0]?.isIntersecting && store.conversations[userId]) {
+        if (store.conversations[userId].length > 0) {
+          fetchNextMessages();
+        }
+      }
+    },
+    { threshold: 0.5 },
+  );
+
+  const fetchNextMessages = async () => {
+    // fetch next messages
+    setTimeout(() => {
+      alert("fetching next messages");
+    }, 1000);
+  };
+
   return (
     <div className="flex max-h-[89vh] flex-1 flex-col overflow-y-hidden bg-neo-gray-hover md:p-6">
       <div className="flex-1 overflow-y-scroll p-4">
         {(store.conversations[userId] ?? []).map((message, ind) => (
           <Message key={ind} message={message} />
         ))}
+        <div ref={observerRef}></div>
       </div>
       <div className="bg-neo-gray">
         <div className="flex">

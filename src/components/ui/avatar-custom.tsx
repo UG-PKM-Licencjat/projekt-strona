0f6665f0503b132b0342b5594e7649564e8dc2fd
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-09-26 14:55:38 +0200

    Change avatar to export the same components and usage as the previous one

--- src/components/ui/avatar-custom.tsx
+++ src/components/ui/avatar-custom.tsx
@@ -1,40 +1,53 @@
 import Image, { type ImageProps } from "next/image";
 import { cn } from "~/lib/utils";
 
-interface AvatarProps extends ImageProps {
-  fallback?: React.ReactNode;
-}
+type AvatarProps = React.ComponentPropsWithRef<"div">;
 
-export const Avatar = ({
-  fallback,
-  src,
-  alt,
-  className,
-  ...props
-}: AvatarProps) => {
+const Avatar = ({ children, className, ...props }: AvatarProps) => {
   return (
     <div
       className={cn(
         "relative h-10 w-10 overflow-hidden rounded-full",
         className,
       )}
+      {...props}
     >
-      <Image
-        src={src}
-        alt={alt}
-        width={100}
-        height={100}
-        referrerPolicy="no-referrer"
-        {...props}
-        className="absolute z-50 aspect-square h-full w-full"
-      />
-      {fallback ? (
-        <div className="absolute inset-0 z-10 flex h-full w-full items-center justify-center rounded-full bg-neo-sage text-black">
-          {fallback}
-        </div>
-      ) : (
-        <div className="absolute inset-0 z-10 flex h-full w-full animate-pulse items-center justify-center rounded-full bg-neo-sage"></div>
-      )}
+      {children}
     </div>
   );
 };
+
+interface AvatarImageProps extends ImageProps {
+  fallback?: React.ReactNode;
+}
+
+const AvatarImage = ({ src, alt, ...props }: AvatarImageProps) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={64}
+      height={64}
+      referrerPolicy="no-referrer"
+      {...props}
+      className="absolute z-50 aspect-square h-full w-full"
+    />
+  );
+};
+
+const AvatarFallback = ({ children, className, ...props }: AvatarProps) => {
+  return (
+    <div
+      className={cn(
+        "absolute inset-0 z-10 flex h-full w-full items-center justify-center rounded-full bg-neo-sage text-black",
+        children ? "" : "animate-pulse",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { Avatar, AvatarImage, AvatarFallback };

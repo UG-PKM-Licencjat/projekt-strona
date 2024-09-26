a7ab79cbbbdfc17c0bb31e103f6ab48106336924
piotrselak <piotr.selak.it@gmail.com>
2024-09-26 14:41:28 +0200

    Added label if user in chat is an artist

--- src/server/api/routers/user/fetchMany.ts
+++ src/server/api/routers/user/fetchMany.ts
@@ -1,14 +1,20 @@
 import { z } from "zod";
 import { authedProcedure } from "../../trpc";
-import { users } from "~/server/db/schema";
-import { inArray } from "drizzle-orm";
+import { offers, users } from "~/server/db/schema";
+import { eq, inArray } from "drizzle-orm";
 
 const fetchManyProcedure = authedProcedure
   .input(z.array(z.string())) // Array of ids
   .query(async ({ ctx, input }) => {
     return await ctx.db
-      .select({ id: users.id, name: users.name, image: users.image })
+      .select({
+        id: users.id,
+        name: users.name,
+        image: users.image,
+        offerId: offers.id,
+      })
       .from(users)
+      .leftJoin(offers, eq(offers.userId, users.id))
       .where(inArray(users.id, input));
   });
 
